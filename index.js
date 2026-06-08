715e209ab3bd9dcf41a7746cd3173d9636f58e8f
Samriddho Kar <samriddhokar8@gmail.com>
2026-06-08 22:15:41 +0530

    built dynamic blog creation and routing

--- index.js
+++ index.js
@@ -10,30 +10,46 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
+const posts = [];
+
 app.get("/", (req,res) => {
     res.render("index.ejs" , {
     posts : posts
     });
+});
+app.get("/", (req,res) => {
+    res.render("index.ejs", { posts });
 });
 
 app.get("/new", (req,res)=>{
     res.render("new.ejs");
 });
 
+   
 app.post("/create", (req,res) => {
     const {title, content} = req.body;
-    const posts = [];
     const newPost = {
+    id:posts.length,
     title: title,
     content: content
     };
     posts.push(newPost);
     console.log(posts);
-    res.render("index.ejs",{posts});
+    res.redirect("/");
 });
 
+app.get("/post/:id", (req,res)=>{
+    const post = posts.find(
+        p => p.id == req.params.id
+    );
 
+    res.render("post.ejs", { post });
+});
 
 app.listen(port, (req, res) => {
     console.log(`Server running on port ${port}.`);
 });
+
+function showForm(){
+    document.getElementById("postForm").style.display = "flex";
+}
